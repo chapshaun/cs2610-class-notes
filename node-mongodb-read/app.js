de9bd632c28fd14639573fa756798e9cb939e20c
Shaun Chap <chapshaun@yahoo.com>
2015-11-30 09:10:51 -0700

    update

--- node-mongodb-read/app.js
+++ node-mongodb-read/app.js
@@ -6,6 +6,8 @@ var assert = require('assert')
 
 var db = require('./db')
 
+var Users = require('./models/users')
+
 var app = express()
 
 app.engine('handlebars', exphbs({defaultLayout: 'base'}));
@@ -18,34 +20,11 @@ app.get('/', function(req, res) {
 })
 
 app.post('/', function(req, res) {
-  // // Connection URL
-  // var url = 'mongodb://dbuser:password@ds055574.mongolab.com:55574/testing'
-  // //Use connect method to connect to the Server
-  // MongoClient.connect(url, function(err, db) {
-  //   assert.equal(null, err)
-  //   console.log('Connected correctly to server')
-
   var user = req.body
-  insertUser(user, function() {
-      // db.close()
+  Users.insert(user, function() {
   res.send('It worked!')
     })
-  // })
 })
-
-
-var insertUser = function(user, callback) {
-  // Get the users collection
-  var collection = db.get().collection('users')
-  // Insert a user
-  collection.insert(user, function(err, result) {
-    assert.equal(err, null)
-    assert.equal(1, result.result.n)
-    assert.equal(1, result.ops.length)
-    console.log('Inserted 1 document into the users collection')
-    callback(result)
-  })
-}
 
 db.connect('mongodb://dbuser:password@ds055574.mongolab.com:55574/testing', function(err) {
   if (err) {
